7f2ce24417e8fb02302c67115c3cb6b0a3bdf47d
HritvikShrivastava <87874975+HritvikShrivastava@users.noreply.github.com>
2021-08-03 12:22:54 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+var weight = [35, 38, 40, 45]
+var sum = weight[0] + weight[1] + weight[2] + weight[3]
+var average = sum/4
+console.log(average)
+
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
